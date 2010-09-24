2bafd6c3ff9770a97e8534a013ec693c6f294cad
Eric di Domenico <eric@didomenico.com>
2010-09-23 20:03:40 -0700

    Intial commit

---
+++ create.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+if ARGV.size < 1
+   puts 'Usage: create <template name> [var=value...]'
+   exit(1)
+end
+
+template_name = ARGV.first
+ARGV.shift
+
+template_values = Object.new
+class << template_values
+   def init
+      ARGV.each do |arg|
+         arg_array = arg.split('=')
+         eval("@#{arg_array[0]} = '#{arg_array[1]}'")
+      end
+   end
+
+   def get_binding
+      binding
+   end
+end
+
+template_values.init
+
+template = IO.readlines(File.dirname(__FILE__) + "/../lib/templates/#{template_name}.erb").to_s
+
+result = ERB.new(template)
+
+result.run(template_values.get_binding)
