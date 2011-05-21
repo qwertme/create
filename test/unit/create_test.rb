4ad575594e7a3d47acafae37e867150ea08645e2
Eric di Domenico <eric@didomenico.com>
2011-05-21 19:43:26 -0400

    Enhance unit tests

--- test/unit/create_test.rb
+++ test/unit/create_test.rb
@@ -2,23 +2,22 @@ require 'test/unit'
 
 class CreateTest < Test::Unit::TestCase
   CREATE_ROOT =  File.join(File.dirname(__FILE__), '..', '..')
-  def test_create
 
-    assert_template('unit_test', 'some_unit_test')
-    assert_template('controller_test', 'some_controller_test')
-    assert_template('ruby_class', 'some_ruby')
-    assert_template('ruby_class', 'some_ruby_with_variables', 'variables' => 'a,b,c')
-    assert_template('active_support_test', 'some_rails_test', 'name' => 'SomeRails')
-    assert_template('factory', 'some_factory', 'name' => 'some_factory')
-    assert_template('helper_test', 'some_helper_test', 'name' => 'SomeHelper')
+  def test_create
+    assert_template('unit_test', 'some_unit_test.rb', :name => 'SomeUnitTest')
+    assert_template('controller_test', 'some_controller_test.rb', :name => 'SomeControllerTest')
+    assert_template('ruby_class', 'some_ruby.rb', :name => 'SomeRuby')
+    assert_template('ruby_class', 'some_ruby_with_variables.rb', :name => 'SomeRubyWithVariables',:variables => 'a,b,c')
+    assert_template('active_support_test', 'some_rails_test.rb', :name => 'SomeRails')
+    assert_template('factory', 'some_factory.rb', :name => 'some_factory')
+    assert_template('helper_test', 'some_helper_test.rb', :name => 'SomeHelper')
   end
 
   private
-  def assert_template(test_type, template_name, options={})
+  def assert_template(template_name, fixture_name, options={})
     create = File.join(CREATE_ROOT, 'bin', 'create.rb')
-    template_file = File.join(CREATE_ROOT, 'test', 'fixtures', "#{template_name}.rb")
-    options['name'] = template_name.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } unless options.has_key?('name')
+    template_file = File.join(CREATE_ROOT, 'test', 'fixtures', "#{fixture_name}")
 
-    assert_equal IO.readlines(template_file).join, `#{create} #{test_type} #{options.collect {|k,v| "#{k}=#{v}"}.join(' ')}`
+    assert_equal IO.readlines(template_file).join, `#{create} #{template_name} #{options.collect {|k,v| "#{k.to_s}=#{v}"}.join(' ')}`
   end
 end
